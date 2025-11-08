1ae7d2db76e89460b77c78abeaa31b315c969fa7
Oursdingo <sawadogosharif20@gmail.com>
2025-11-08 17:25:11 +0000

    ff

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['three'],
+  transpilePackages: ["three"],
   images: {
-    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     minimumCacheTTL: 31536000,
   },
-  // Uncomment for static export
-  // output: 'export',
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
